a27ba3254e4eddae7a09a0896273ec7a990031d9
I'm Not A Bot #Left_TG <77770753+Itz-fork@users.noreply.github.com>
2021-08-13 18:35:55 +0530

    nice

--- pyro_str_gen.py
+++ pyro_str_gen.py
@@ -23,6 +23,7 @@ async def pyro_str():
         pyro_session = await NEXAUB.export_session_string()
         session_msg = await NEXAUB.send_message("me", f"`{pyro_session}`")
         await session_msg.reply_text("Successfully Generated String Session! Thanks for trying [Nexa Userbot](https://github.com/Itz-fork/Nexa-Userbot) \n\n**Join @NexaBotsUpdates**", disable_web_page_preview=True)
+        print("\nString Session has been sent to your saved messages. Please check it. Thank You!\n")
 
 if __name__ == "__main__":
     loop = asyncio.get_event_loop()
