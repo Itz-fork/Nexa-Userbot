489cf1cee3eb6aa595b072a7719cc12caf1b1069
I'm Not A Bot #Left_TG <77770753+Itz-fork@users.noreply.github.com>
2021-08-14 17:13:24 +0530

    Update __main__.py

--- pyrogram_ub/__main__.py
+++ pyrogram_ub/__main__.py
@@ -1,7 +1,11 @@
 # Copyright (c) 2021 Itz-fork
 # Part of: Nexa-Userbot
+import os
 from pyrogram_ub import NEXAUB
 from pyrogram_ub.modules import *
+from config import Config
 
 print("Pyrogram Client Has Started! \n\nPowered by Nexa UserBot")
+if not os.path.isdir(Config.DOWNLOAD_LOCATION):
+  os.makedirs(Config.DOWNLOAD_LOCATION)
 NEXAUB.run()
