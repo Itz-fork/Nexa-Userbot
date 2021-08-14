90a26cec8014ba30dca46bc179ad06f92e51350d
I'm Not A Bot #Left_TG <77770753+Itz-fork@users.noreply.github.com>
2021-08-14 17:23:51 +0530

    Update py_extract.py

--- pyrogram_ub/modules/py_extract.py
+++ pyrogram_ub/modules/py_extract.py
@@ -25,7 +25,7 @@ CMD_HELP.update(
 async def extract_all_aud(_, message: Message):
     replied_msg = message.reply_to_message
     ext_text = await message.edit("`Processing...`")
-    ext_out_path = os.getcwd() + "/" + "py_extract/audios"
+    ext_out_path = os.getcwd() + "/" + "py_extract/NexaUB/audios"
     if not replied_msg:
         await ext_text.edit("`Please reply to a valid video file!`")
         return
@@ -40,7 +40,7 @@ async def extract_all_aud(_, message: Message):
         await ext_text.edit("`Downloading...`")
         ext_video = await NEXAUB.download_media(message=replied_video)
         await ext_text.edit("`Extracting Audio(s)...`")
-        exted_aud = Video_tools.extract_all_audio(input_file=ext_video)
+        exted_aud = Video_tools.extract_all_audio(input_file=ext_video, output_path=ext_out_path)
         await ext_text.edit("`Extracting Finished! Now Uploading to Telegram!`")
         for nexa_aud in exted_aud:
             await message.reply_audio(audio=nexa_aud, caption=f"`Extracted by` {(await NEXAUB.get_me()).mention}")
