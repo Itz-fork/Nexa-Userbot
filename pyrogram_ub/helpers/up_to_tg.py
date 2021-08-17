2511b2617eb434ee2e5e9c9f56bc3ee73ee79bb7
I'm Not A Bot #Left_TG <77770753+Itz-fork@users.noreply.github.com>
2021-08-17 10:30:50 +0530

    Update up_to_tg.py

--- pyrogram_ub/helpers/up_to_tg.py
+++ pyrogram_ub/helpers/up_to_tg.py
@@ -17,18 +17,21 @@ async def guess_and_send(input_file, chat_id, thumb_path):
 #     if not guessedfilemime.mime:
 #         await NEXAUB.send_document(chat_id=chat_id, document=in_file, caption=f"`Uploaded by` {(await NEXAUB.get_me()).mention}")
 #         return
-    filemimespotted = guessedfilemime.mime
-    if "image/gif" in filemimespotted:
+    try:
+        filemimespotted = guessedfilemime.mime
+        if "image/gif" in filemimespotted:
+            await NEXAUB.send_animation(chat_id=chat_id, animation=in_file, caption=f"`Uploaded by` {(await NEXAUB.get_me()).mention}")
+        elif "image" in filemimespotted:
+            await NEXAUB.send_photo(chat_id=chat_id, photo=in_file, caption=f"`Uploaded by` {(await NEXAUB.get_me()).mention}")
+        elif "video" in filemimespotted:
+            viddura = get_vid_duration(input_video=in_file)
+            thumbnail_path = f"{thumbnail_bpath}/thumbnail.jpg"
+            subprocess.call(['ffmpeg', '-i', in_file, '-ss', '00:00:00.000', '-vframes', '1', thumbnail_path])
+            await NEXAUB.send_video(chat_id=chat_id, video=in_file, thumb=thumbnail_path, duration=viddura, caption=f"`Uploaded by` {(await NEXAUB.get_me()).mention}")
+        elif "audio" in filemimespotted:
+            await NEXAUB.send_audio(chat_id=chat_id, audio=in_file, caption=f"`Uploaded by` {(await NEXAUB.get_me()).mention}")
+        else:
+            await NEXAUB.send_document(chat_id=chat_id, document=in_file, caption=f"`Uploaded by` {(await NEXAUB.get_me()).mention}")
+    except Exception as e:
+        print(e)
         await NEXAUB.send_animation(chat_id=chat_id, animation=in_file, caption=f"`Uploaded by` {(await NEXAUB.get_me()).mention}")
-        return
-    elif "image" in filemimespotted:
-        await NEXAUB.send_photo(chat_id=chat_id, photo=in_file, caption=f"`Uploaded by` {(await NEXAUB.get_me()).mention}")
-    elif "video" in filemimespotted:
-        viddura = get_vid_duration(input_video=in_file)
-        thumbnail_path = f"{thumbnail_bpath}/thumbnail.jpg"
-        subprocess.call(['ffmpeg', '-i', in_file, '-ss', '00:00:00.000', '-vframes', '1', thumbnail_path])
-        await NEXAUB.send_video(chat_id=chat_id, video=in_file, caption=f"`Uploaded by` {(await NEXAUB.get_me()).mention}")
-    elif "audio" in filemimespotted:
-        await NEXAUB.send_audio(chat_id=chat_id, audio=in_file, caption=f"`Uploaded by` {(await NEXAUB.get_me()).mention}")
-    else:
-        await NEXAUB.send_document(chat_id=chat_id, document=in_file, caption=f"`Uploaded by` {(await NEXAUB.get_me()).mention}")
