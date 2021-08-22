50c32931b77af514187d25647e2d7a570f25980a
I'm Not A Bot #Left_TG <77770753+Itz-fork@users.noreply.github.com>
2021-08-23 01:06:01 +0530

    tf

--- nexa_userbot/modules/ub_updater.py
+++ nexa_userbot/modules/ub_updater.py
@@ -100,8 +100,8 @@ async def upstream(client, message):
         pass
     ups_rem = repo.remote("upstream")
     ups_rem.fetch(ac_br)
+    changelog = await gen_chlog(repo, f"HEAD..upstream/{ac_br}")
     if "now" not in conf:
-        changelog = await gen_chlog(repo, f"{ac_br}")
         if changelog:
             changelog_str = f"**New Updates available for Nexa Userbot** \n\n**Branch:** [{ac_br}]({UPSTREAM_REPO_URL}/tree/{ac_br}) \n\n**Changelog (last 10):** \n\n{changelog}"
             if len(changelog_str) > 4096:
