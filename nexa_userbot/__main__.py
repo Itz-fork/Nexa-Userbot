72ff4d69320636e51c8adc93b233f89892596242
I'm Not A Bot #Left_TG <77770753+Itz-fork@users.noreply.github.com>
2021-08-30 21:50:37 +0530

    added arq module
    - Added Translator
    - Added Lyrics finder
    - Added wiki searcher

--- nexa_userbot/__main__.py
+++ nexa_userbot/__main__.py
@@ -5,7 +5,7 @@ import asyncio
 from pyrogram import idle
 from nexa_userbot import NEXAUB
 from nexa_userbot.modules import *
-from nexa_userbot.core.startup_checks import check_or_set_log_channel
+from nexa_userbot.core.startup_checks import check_or_set_log_channel, check_arq_api
 from nexa_userbot.core.nexaub_database.nexaub_db_conf import get_log_channel
 from config import Config
 
@@ -19,6 +19,7 @@ Copyright (c) 2021 Itz-fork
     )
     await NEXAUB.start()
     await check_or_set_log_channel()
+    await check_arq_api()
     log_channel_id = await get_log_channel()
     await NEXAUB.send_message(chat_id=log_channel_id, text="`Nexa Userbot is started!`")
     await idle()
