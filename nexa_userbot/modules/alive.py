19442b3b35225e9420eddf36cf3654bfa70b5fda
Itz-fork <git.itzfork@gmail.com>
2021-12-07 08:32:45 +0530

    Added support for custom alive pic

--- nexa_userbot/modules/alive.py
+++ nexa_userbot/modules/alive.py
@@ -13,9 +13,10 @@ from sys import version_info
 
 from nexa_userbot import NEXAUB, CMD_HELP, StartTime
 from nexa_userbot.helpers.pyrogram_help import get_arg
-from nexa_userbot.core.nexaub_database.nexaub_db_conf import set_custom_alive_msg, get_custom_alive_msg
+from nexa_userbot.core.nexaub_database.nexaub_db_conf import set_custom_alive_msg, get_custom_alive_msg, set_custom_var, get_custom_var
 from nexa_userbot.core.main_cmd import nexaub_on_cmd, e_or_r
 from nexa_userbot.core.startup_checks import check_or_set_log_channel
+from .telegraph import upload_to_tgraph
 from config import Config
 
 
@@ -28,7 +29,7 @@ CMD_HELP.update(
   ✘ `alive` - To Check If Your Nexa Userbot Alive
   ✘ `ping` - To Check Ping Speed
   ✘ `setalive` - To Set Custom Alive Message
-  ✘ `getalive` - To Get 
+  ✘ `getalive` - To Get current alive message
 
 **Example:**
 
@@ -49,7 +50,6 @@ def get_readable_time(seconds: int) -> str:
     ping_time = ""
     time_list = []
     time_suffix_list = ["s", "m", "h", "days"]
-
     while count < 4:
         count += 1
         remainder, result = divmod(seconds, 60) if count < 3 else divmod(seconds, 24)
@@ -57,15 +57,12 @@ def get_readable_time(seconds: int) -> str:
             break
         time_list.append(int(result))
         seconds = int(remainder)
-
     for x in range(len(time_list)):
         time_list[x] = str(time_list[x]) + time_suffix_list[x]
     if len(time_list) == 4:
         ping_time += time_list.pop() + ", "
-
     time_list.reverse()
     ping_time += ":".join(time_list)
-
     return ping_time
 
 # Get current version of Nexa Userbot
@@ -75,13 +72,17 @@ def get_nexaub_version():
         return ver["version"]
 
 
+# Alive Message
 @nexaub_on_cmd(command="alive", modlue=mod_file)
 async def pyroalive(_, message: Message):
     uptime = get_readable_time((time.time() - StartTime))
     alive_bef_msg = await e_or_r(nexaub_message=message, msg_text="`Processing...`")
+    # Alive Message
     get_alive_msg = await get_custom_alive_msg()
     custom_alive_msg = get_alive_msg if get_alive_msg else "Heya, I'm Using Nexa Userbot"
-    alive_pic = "cache/NEXAUB.png"
+    # Alive Pic
+    get_alive_pic = await get_custom_var(var="ALIVE_PIC")
+    alive_pic = get_alive_pic if get_alive_pic else "cache/NEXAUB.png"
     NEXAUB_VERSION = get_nexaub_version()
     alive_msg = f"""
 **{custom_alive_msg}**
@@ -99,16 +100,18 @@ async def pyroalive(_, message: Message):
     await alive_bef_msg.delete()
     await NEXAUB.send_photo(chat_id=message.chat.id, photo=alive_pic, caption=alive_msg)
 
+# Ping
 @nexaub_on_cmd(command="ping", modlue=mod_file)
 async def pingme(_, message: Message):
+    ping_msg = await e_or_r(nexaub_message=message, msg_text="`Processing...`")
     start = datetime.now()
     end = datetime.now()
     ping_time = (end - start).microseconds / 1000
-    ping_msg = await e_or_r(nexaub_message=message, msg_text="`Processing...`")
-    await e_or_r(nexaub_message=message, msg_text=f"**Pong:** `{ping_time} ms`", disable_web_page_preview=True)
+    await ping_msg.edit(nexaub_message=message, msg_text=f"**Pong:** `{ping_time} ms`", disable_web_page_preview=True)
 
+# Set custom alive message
 @nexaub_on_cmd(command="setalive", modlue=mod_file)
-async def pyroalive(_, message: Message):
+async def set_alive(_, message: Message):
     alive_r_msg = await e_or_r(nexaub_message=message, msg_text="`Processing...`")
     c_alive_msg = get_arg(message)
     r_msg = message.reply_to_message
@@ -116,23 +119,37 @@ async def pyroalive(_, message: Message):
         if r_msg:
             c_alive_msg = r_msg.text
         else:
-            await e_or_r(nexaub_message=message, msg_text="`Please reply to a text message!`")
-            return
+            return await alive_r_msg.edit(nexaub_message=message, msg_text="`Please reply to a text message!`")
     await set_custom_alive_msg(a_text=c_alive_msg)
-    await e_or_r(nexaub_message=message, msg_text="`Successfully Updated Custom Alive Message!`")
+    await alive_r_msg.edit(nexaub_message=message, msg_text="`Successfully Updated Custom Alive Message!`")
 
+# Get custom alive message
 @nexaub_on_cmd(command="getalive", modlue=mod_file)
-async def pyroalive(_, message: Message):
+async def get_alive(_, message: Message):
     g_alive_msg = await e_or_r(nexaub_message=message, msg_text="`Processing...`")
     try:
         get_al = await get_custom_alive_msg()
         saved_alive_msg = get_al if get_al else "No Custom Message is saved!"
-        await g_alive_msg.edit(f"**Current Alive Message:** \n{get_al}")
+        await g_alive_msg.edit(f"**Current Alive Message:** \n{saved_alive_msg}")
     except Exception as e:
         print(e)
 
+# Set custom alive picture
+@nexaub_on_cmd(command="setalivepic", modlue=mod_file)
+async def set_alive_pic(_, message: Message):
+    cust_alive = await e_or_r(nexaub_message=message, msg_text="`Processing...`")
+    r_msg = message.reply_to_message
+    if r_msg.photo or r_msg.animation:
+        alive_pic = r_msg.download()
+        alive_url = upload_to_tgraph(alive_pic)
+        await set_custom_var(var="ALIVE_PIC", value=alive_url)
+        await cust_alive.edit(f"`Successfully Saved Custom Alive Picture!` \n\n**Preview:** [Click here]({alive_url})")
+    else:
+        await cust_alive.edit("`Reply to a photo or gif 😑!`")
+
+
 @nexaub_on_cmd(command="clc", modlue=mod_file)
-async def pyroalive(_, message: Message):
+async def egg_clc(_, message: Message):
     clc_func = await check_or_set_log_channel()
     lc_id = clc_func[1] if clc_func[1]  else None
     await e_or_r(nexaub_message=message, msg_text=f"**Is Log Channel Set?** `{clc_func[0]}` \n**Channel ID:** `{lc_id}`")
--- nexa_userbot/modules/telegraph.py
+++ nexa_userbot/modules/telegraph.py
@@ -2,7 +2,7 @@
 # Part of: Nexa-Userbot
 import os
 
-from telegraph import Telegraph, upload_file
+from telegraph import Telegraph
 from pyrogram.types import Message
 
 from nexa_userbot import NEXAUB, CMD_HELP
@@ -44,42 +44,46 @@ async def paste_text_to_tgraph(title, text):
     if title is None:
       title = f_name if f_name is not None else "By Nexa Userbot"
     t_response = telegraph.create_page(title=title, html_content=text, author_name=f_name if f_name is not None else "Nexa-Userbot", author_url=f"https://t.me/{u_name}" if u_name is not None else "https://github.com/Itz-fork/Nexa-Userbot")
-    return f"**Telegraph Link:** {t_response['url']}"
+    return f"{t_response['url']}"
   except Exception as e:
     return f"**Error:** {e}"
 
-# Upload image to telegraph
+# Upload media to telegraph
 async def upload_to_tgraph(file):
   try:
-    t_response = upload_file(file)
-    return f"**Telegraph Link:** https://telegra.ph/{t_response[0]}"
+    t_response = telegraph.upload_file(file)[0]["src"]
+    return f"https://telegra.ph/{t_response}"
   except Exception as e:
     return f"**Error:** {e}"
 
 
 @nexaub_on_cmd(command="telegraph", modlue=mod_file)
-async def me_goin_oflin(_, message: Message):
+async def telegraph_up(_, message: Message):
     tgraph_msg = await e_or_r(nexaub_message=message, msg_text="`Processing...`")
     r_msg = message.reply_to_message
     arg_txt = get_arg(message)
     if r_msg:
-        if r_msg.photo or r_msg.video or r_msg.video_note:
-          r_content = await r_msg.download()
-          up_to_tgraph = await upload_to_tgraph(r_content)
-          await tgraph_msg.edit(up_to_tgraph)
-        elif r_msg.sticker:
-          r_content = await convert_to_image(message=r_msg, client=NEXAUB)
-          up_to_tgraph = await upload_to_tgraph(r_content)
-          await tgraph_msg.edit(up_to_tgraph)
-        elif r_msg.text:
-          r_content = r_msg.text
-          if arg_txt:
-            t_title = arg_txt
-          else:
-            t_title = None
-          t_pasted = await paste_text_to_tgraph(title=t_title, text=r_content)
-          await tgraph_msg.edit(t_pasted)
+      # Photo / Video or Video note
+      if r_msg.photo or r_msg.video or r_msg.video_note:
+        r_content = await r_msg.download()
+      # Stickers
+      elif r_msg.sticker:
+        r_content = await convert_to_image(message=r_msg, client=NEXAUB)
+      # Text messages
+      elif r_msg.text:
+        r_content = r_msg.text
+        # Set title if provided by user
+        if arg_txt:
+          t_title = arg_txt
         else:
-          tgraph_msg.edit("`No Supported Media or Text to paste!`")
+          t_title = None
+        # Paste text to telegraph
+        t_pasted = await paste_text_to_tgraph(title=t_title, text=r_content)
+      else:
+        tgraph_msg.edit("`No Supported Media or Text to paste!`")
+      # Paste media to telegraph
+      t_pasted = await upload_to_tgraph(r_content)
+      # Edit message with the telegraph link
+      await tgraph_msg.edit(f"**Telegraph Link:** {t_pasted}")
     else:
       return await tgraph_msg.edit("Reply to a message that contains `text`/`image` or `mp4 file`!")
