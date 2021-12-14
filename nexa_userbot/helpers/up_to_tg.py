d9591c8811dd059139691380e8b68409b186d45c
Itz-fork <git.itzfork@gmail.com>
2021-12-14 14:43:51 +0530

    Search for mime type using regex

--- nexa_userbot/helpers/up_to_tg.py
+++ nexa_userbot/helpers/up_to_tg.py
@@ -1,6 +1,7 @@
 # Copyright (c) 2021 Itz-fork
 # Part of: Nexa Userbot
 import os
+import re
 import filetype
 from .pyrogram_help import run_shell_cmds
 
@@ -18,18 +19,22 @@ async def guess_and_send(input_file, chat_id, thumb_path):
         return await NEXAUB.send_document(chat_id=chat_id, document=in_file, caption=f"`Uploaded by` {(await NEXAUB.get_me()).mention}")
     try:
         filemimespotted = guessedfilemime.mime
-        if "image/gif" in filemimespotted:
+        # For gifs
+        if re.search(r'\bimage/gif\b', filemimespotted):
             await NEXAUB.send_animation(chat_id=chat_id, animation=in_file, caption=f"`Uploaded by` {(await NEXAUB.get_me()).mention}")
-        elif "image" in filemimespotted:
+        # For images
+        elif re.search(r'\bimage\b', filemimespotted):
             await NEXAUB.send_photo(chat_id=chat_id, photo=in_file, caption=f"`Uploaded by` {(await NEXAUB.get_me()).mention}")
-        elif "video" in filemimespotted:
+        # For videos
+        elif re.search(r'\bvideo\b', filemimespotted):
             viddura = await get_vid_duration(input_video=in_file)
             thumbnail_path = f"{thumbnail_bpath}/thumbnail_{os.path.basename(in_file)}.jpg"
             if os.path.exists(thumbnail_path):
                 os.remove(thumbnail_path)
             await run_shell_cmds(f"ffmpeg -i {in_file} -ss 00:00:01.000 -vframes 1 {thumbnail_path}")
             await NEXAUB.send_video(chat_id=chat_id, video=in_file, duration=viddura, thumb=thumbnail_path, caption=f"`Uploaded by` {(await NEXAUB.get_me()).mention}")
-        elif "audio" in filemimespotted:
+        # For audio
+        elif re.search(r'\baudio\b', filemimespotted):
             await NEXAUB.send_audio(chat_id=chat_id, audio=in_file, caption=f"`Uploaded by` {(await NEXAUB.get_me()).mention}")
         else:
             await NEXAUB.send_document(chat_id=chat_id, document=in_file, caption=f"`Uploaded by` {(await NEXAUB.get_me()).mention}")
